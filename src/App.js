c7c3c0e44fb9547d55806e8f7adb54259d69185b
cps97 <colinstoutt@gmail.com>
2023-01-07 10:45:10 -0800

    add header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
-import './App.css';
-import Footer from './components/Footer';
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <Footer/>
-      
+      <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <h1 className="title">Header</h1>;
+};
+
+export default Header;
